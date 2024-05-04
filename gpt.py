99643d2dfc1adb2070f9811cef56d45d124edfaa
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-05-04 13:33:57 -0500

    Updates to gpt initial prompt

--- gpt.py
+++ gpt.py
@@ -10,7 +10,7 @@ def translate( texto: str ) -> str:
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
-            {"role": "system", "content": "Translate from english to spanish. Preserve html tags and information. The following text is all to be translated>>"},
+            {"role": "system", "content": "Translate from english to spanish. Preserve html tags and information. DO NOT TRANSLATE LINKS, WRITE THEM THE SAME WAY. And respect the beggining and endings of phrases: If it beggins in lowercase, the ending punctuation (. , ! etc). The following text is all to be translated>>"},
             {"role":"user","content": texto}
         ]
     )
